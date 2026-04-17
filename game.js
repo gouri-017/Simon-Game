ed0c9639e27cf959d092bde2ccbbb88d8c534181
Gouri <gouri7136@gmail.com>
2026-04-17 21:20:02 +0530

    final working version

--- game.js
+++ game.js
@@ -1,35 +1,101 @@
-function r()
-{
-    let ran =Math.round (Math.random() * 3)
-    console.log(ran)
-    return ran;
-}
-gamepattern =[]
-  const buttonColours = ['red','blue','green','yellow']
-  
-  let randomChosenColour = buttonColours[r()]
-  console.log(randomChosenColour)
-  gamepattern.push(randomChosenColour)
-  console.log(gamepattern);
+const buttonColours = ['red','blue','green','yellow']
+let gamepattern =[]
+let userClickedPattern =[]
+let allButtons = document.querySelectorAll('.btn')
+started = false
+level = 0
 
-  const selectedButton = document.getElementById(`${randomChosenColour}`)
+function nextSequence()
+{
+  level++;
+  document.querySelector("#level-title").textContent = `Level ${level}`;
+    let randomNum = Math.round (Math.random() * 3)
+    // console.log(randomNum)
+    let randomChosenColour = buttonColours[randomNum]
+    gamepattern.push(randomChosenColour);
+    console.log(`game pattern = ${gamepattern}`)
+    userClickedPattern = []
+    return randomChosenColour;
+}
+
+function playSound(name){
+  var audio = new Audio(`${name}.mp3`)
+  audio.play()
+  if (name === 'wrong')
+    return;
+  selectedButton = document.getElementById(`${name}`)
   console.log(selectedButton);
+  
   selectedButton.animate([
   { backgroundColor: 'transparent' },
-  { backgroundColor: `light ${randomChosenColour}` },
+  { backgroundColor: `${name}` },
   { backgroundColor: 'transparent' }
 ], {
-  duration: 200,
+  duration: 100,
   iterations: 2
-});
-// const audio = new Audio(`${randomChosenColour}.mp3`)
-const audio = new Audio(`${randomChosenColour}.mp3`)
+}); 
+}
 
-audio.play()
-  
-// const greenButton = document.querySelector('#green')
-// const redButton = document.querySelector('#red')
-// const yellowButton = document.querySelector('#yellow')
-// const blueButton = document.querySelector('#blue')
+function startOver(){
+  document.querySelector('#level-title').textContent = 'Game Over! Press any key to restart.'
+  level = 0;
+  gamepattern = []
+  userClickedPattern = []
+  started = false
+  return;
+}
 
-// 3 ways to add flash to a button either use jquery or togglee css classes means create a css class with flas add or remove that with button click event handling or use .animate method
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] !== gamepattern[currentLevel])
+// console.log('working');
+    {
+      console.log('nope');
+      playSound("wrong");
+      document.body.classList.add('game-over')
+      setTimeout(()=>{
+        document.body.classList.remove('game-over')
+      },200)
+      startOver()
+      return;
+    }     
+}
+
+// ------------------------------------execution starts from heeree-------------------------
+// when a keyboard key is pressed 
+
+for (let i = 0; i < allButtons.length; i++) {
+  allButtons[i].addEventListener("click", (e) => {
+    // console.log(e.target);
+    userChosenColor = e.target.id
+    userClickedPattern.push(userChosenColor)
+    playSound(userChosenColor);
+    //check here
+    let ind = userClickedPattern.length - 1
+    checkAnswer(ind);
+    // that;
+    console.log(userClickedPattern);
+    console.log(started);
+    
+    if (started && userClickedPattern.length === gamepattern.length){
+      setTimeout(() => {
+  playSound(nextSequence());
+},1000);
+    }
+    // else{
+    //   document.body.classList.add('game-over')
+    // }
+ 
+  });
+
+}
+
+document.addEventListener("keypress",()=>{
+
+if (!started){
+  started = true
+  if (level===0){
+    document.querySelector("#level-title").textContent = `Level 0`;
+    playSound(nextSequence()); }
+
+}
+})
